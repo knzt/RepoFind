5d5f9efe0c0f00380ed0b168212da11d676cefa5
knzt <cristinahellen46@gmail.com>
2023-05-01 22:29:02 -0300

    add and styled input and button (pages/Home)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,31 @@
 import { Header } from '../../components/Header';
 
+import github from '../../assets/github.png';
+
+import './styles.css';
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <div className='content'>
+        <img src={github} alt="Background com ícone do Github" className='background'/>
+
+        <div className='info'>
+          <div>
+            <input name='user' placeholder='@username' />
+
+            <button>
+              Buscar
+              <span class="top"></span>
+              <span class="right"></span>
+              <span class="bottom"></span>
+              <span class="left"></span>
+            </button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
